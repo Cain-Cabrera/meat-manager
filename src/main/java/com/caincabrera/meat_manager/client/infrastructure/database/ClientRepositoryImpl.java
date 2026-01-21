00ab7fc4930a6222064c2cb7e5863143a8274001
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-01-21 00:39:50 -0300

    primeros pasos con Arquitectura Hexagonal, creando packages y interfaces.

--- src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/ClientRepositoryImpl.java
+++ src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/ClientRepositoryImpl.java
@@ -2,13 +2,11 @@ package com.caincabrera.meat_manager.client.infrastructure.database;
 
 import com.caincabrera.meat_manager.client.domain.Client;
 import com.caincabrera.meat_manager.client.domain.ClientRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public class ClienteRepositoryImpl implements ClientRepository {
+public class ClientRepositoryImpl implements ClientRepository {
     @Override
     public void upsert(Client client) {
 
