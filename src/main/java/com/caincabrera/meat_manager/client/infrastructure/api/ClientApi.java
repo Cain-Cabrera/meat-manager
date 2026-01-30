0f596bffa7e92fe04591804bffce3a532b30cc2f
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-01-30 14:05:01 -0300

    Se reemplazo el retorno de cada contrato para devolver el correspondiente dto, de a poco a medida que la app avanza y vamos implementando cada dto, se va a modificar.

--- src/main/java/com/caincabrera/meat_manager/client/infrastructure/api/ClientApi.java
+++ src/main/java/com/caincabrera/meat_manager/client/infrastructure/api/ClientApi.java
@@ -1,19 +1,21 @@
 package com.caincabrera.meat_manager.client.infrastructure.api;
 
-import com.caincabrera.meat_manager.client.domain.Client;
+import com.caincabrera.meat_manager.client.infrastructure.api.dto.ClientDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 public interface ClientApi {
-    //luego va a cambiar los voids, cuando creemos los dtos..
-    ResponseEntity<Void> saveClient(@RequestBody Client client);
 
-    ResponseEntity<Void> updateClient(@PathVariable Long id, @RequestBody Client client);
+    ResponseEntity<ClientDto> saveClient(@RequestBody ClientDto clientDto);
 
-    ResponseEntity<Void> getAllClient();
+    ResponseEntity<ClientDto> updateClient(@PathVariable Long id, @RequestBody ClientDto clientDto);
 
-    ResponseEntity<Void> getByidClient(@PathVariable Long id);
+    ResponseEntity<List<ClientDto>> getAllClient();
+
+    ResponseEntity<ClientDto> getByIdClient(@PathVariable Long id);
 
     ResponseEntity<Void> deleteClient(@PathVariable Long id);
 
