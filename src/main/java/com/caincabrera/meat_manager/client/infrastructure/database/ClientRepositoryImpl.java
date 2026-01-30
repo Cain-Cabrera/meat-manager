541c174bd74401d7b8d5b44656e5c95a97154e49
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-01-30 14:14:15 -0300

    fix: se agrego en memoria una lista para probar cada metodo de persistencia. se busca implementar streams() para mejor codigo.

--- src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/ClientRepositoryImpl.java
+++ src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/ClientRepositoryImpl.java
@@ -2,31 +2,47 @@ package com.caincabrera.meat_manager.client.infrastructure.database;
 
 import com.caincabrera.meat_manager.client.domain.Client;
 import com.caincabrera.meat_manager.client.domain.ClientRepository;
+import com.caincabrera.meat_manager.client.infrastructure.database.entity.ClientEntity;
+import com.caincabrera.meat_manager.client.infrastructure.database.mapper.ClientEntityMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class ClientRepositoryImpl implements ClientRepository {
+
+    private final List<ClientEntity> clients = new ArrayList<>();
+
+    private final ClientEntityMapper clientEntityMapper;
 
     @Override
     public void upsert(Client client) {
-
+        ClientEntity clientEntity = clientEntityMapper.mapToEntity(client);
+        clients.add(clientEntity);
     }
 
     @Override
     public List<Client> findAll() {
-        return List.of();
+        return clients.stream()
+                .findFirst()
+                .map(clientEntityMapper::mapToClient)
+                .stream().toList();
     }
 
     @Override
     public void deleteClient(Long id) {
-
+        clients.removeIf(p -> p.getId().equals(id));
     }
 
     @Override
     public Optional<Client> findById(Long id) {
-        return Optional.empty();
+        return clients.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .map(clientEntityMapper::mapToClient);
     }
 }
