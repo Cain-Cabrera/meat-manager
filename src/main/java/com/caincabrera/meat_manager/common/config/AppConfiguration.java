32b937fdfd218ccec1dd319eeeb0d77d61d10930
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-02-04 20:46:02 -0300

    feat: se creo una clase de configuracion, para poder gestionar tanto las tareas programadas como en un futuro si es necesario, la asincronia.

---
+++ src/main/java/com/caincabrera/meat_manager/common/config/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.caincabrera.meat_manager.common.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configurable
+@EnableScheduling
+public class AppConfiguration {
+}
