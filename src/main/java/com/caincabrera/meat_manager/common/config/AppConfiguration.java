f1ec31ae0d774a733b0d5ea19e53b400c66e1587
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-02-06 22:57:45 -0300

    feat: agregamos configuracion @EnableCaching para habilitar cache.

--- src/main/java/com/caincabrera/meat_manager/common/config/AppConfiguration.java
+++ src/main/java/com/caincabrera/meat_manager/common/config/AppConfiguration.java
@@ -1,9 +1,11 @@
 package com.caincabrera.meat_manager.common.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configurable
 @EnableScheduling
+@EnableCaching
 public class AppConfiguration {
 }
