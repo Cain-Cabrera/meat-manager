07059c39af88ccc02be78b0bcdbd17e39be1f3cf
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-02-16 18:53:37 -0300

    style: para futuros test del controlador.

---
+++ src/test/java/com/caincabrera/meat_manager/client/infrastructure/api/ClientControllerTest.java
@@ -0,0 +1,26 @@
+package com.caincabrera.meat_manager.client.infrastructure.api;
+
+import org.junit.jupiter.api.Test;
+
+class ClientControllerTest {
+
+    @Test
+    void saveClient() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void getAllClient() {
+    }
+
+    @Test
+    void getByIdClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+}
