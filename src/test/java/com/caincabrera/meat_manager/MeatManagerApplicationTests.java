277de32135752a55e0c425f356671bf787137115
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-03-06 23:23:45 -0300

    feat: agregamos dependencia spring data jpa, y agregamos el driver postgresql para usar en nuestro proyecto.

--- src/test/java/com/caincabrera/meat_manager/MeatManagerApplicationTests.java
+++ src/test/java/com/caincabrera/meat_manager/MeatManagerApplicationTests.java
@@ -1,10 +1,9 @@
 package com.caincabrera.meat_manager;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+
 @SpringBootTest
 class MeatManagerApplicationTests {
 
