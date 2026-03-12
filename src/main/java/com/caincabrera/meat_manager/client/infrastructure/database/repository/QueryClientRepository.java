8d50884644d14b38bfd0be2ab720e40c2754856b
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-03-11 23:44:08 -0300

    feat: agregamos una consulta personalizada que spring va a estar manejando por debajo creando metodos derivados. Spring gestiona esto por detras.

--- src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/repository/QueryClientRepository.java
+++ src/main/java/com/caincabrera/meat_manager/client/infrastructure/database/repository/QueryClientRepository.java
@@ -4,7 +4,12 @@ import com.caincabrera.meat_manager.client.infrastructure.database.entity.Client
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface QueryClientRepository extends JpaRepository<ClientEntity, Long> {
+
+    Optional<ClientEntity> findByEmail(String email);
+
 }
